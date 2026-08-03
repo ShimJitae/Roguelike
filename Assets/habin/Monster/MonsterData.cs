c3fa00c9fa0a1898d9f1db110e16dd16c94a58c0
habinsong0311-web <habinsong0311@gmail.com>
2026-08-03 10:03:32 +0900

    [Docs]MonsterData 내용 수정
    string 값으로 되어있는걸 float로 수정

--- Assets/habin/Monster/MonsterData.cs
+++ Assets/habin/Monster/MonsterData.cs
@@ -5,11 +5,11 @@ public class MonsterData : ScriptableObject
 {
     [SerializeField] private string monsterId;
     [SerializeField] private string monsterName;
-    [SerializeField] private string monsterMaxHp;
-    [SerializeField] private string monsterAttack;
-    [SerializeField] private string monsterBodyAttack;
-    [SerializeField] private string monsterDefense;
-    [SerializeField] private string monsterExp;
+    [SerializeField] private float monsterMaxHp;
+    [SerializeField] private float monsterAttack;
+    [SerializeField] private float monsterBodyAttack;
+    [SerializeField] private float monsterDefense;
+    [SerializeField] private float monsterExp;
     private bool monsterAlive {  get; set; }
 
 
--- Assets/habin/Monster/MonsterMove.cs
+++ Assets/habin/Monster/MonsterMove.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class MonsterMove : MonoBehaviour
 {
-    Rigidbody2D rb;
-    Animator an;
+    private Rigidbody2D rb;
+    private Animator an;
+    private SpriteRenderer sp;
     [SerializeField] private MonsterData monsterData;
     [Header("이동 및 감지")]
     [SerializeField] private LayerMask playerLayer;
@@ -12,16 +13,29 @@ public class MonsterMove : MonoBehaviour
     [SerializeField] private float detcetRange;
     [SerializeField] private float attackRange;
     [SerializeField] private float moveSpeed;
-
+    
+    private int currentIndex = 0; 
+    private int moveDirection = 1;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         an = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        
+        //포인트가 없을때 움직이지 않음
+        if (movePoints == null || movePoints.Length == 0)
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+        //이동지점 확인
+        Transform targetPoint = movePoints[currentIndex];
+        //이동지점으로 이동
+        Vector2 direction =((Vector2)targetPoint.position - rb.position).normalized;
+
     }
     private void OnDrawGizmos()
     {
