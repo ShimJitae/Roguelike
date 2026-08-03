de2bd6c39d87e3064a20ccc97d54a4bc7b2c0e1c
dlzkap11 <dlzkap11@gmail.com>
2026-08-03 13:29:40 +0900

    [Add] Collision, PlayerAnim 추가
    벽이나 땅에 닿았는지 확인하는 Collision 스크립트, Player의 Animation을 담당할 PlayerAnim 스크립트 추가

--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
-    
+    private SPUM_Prefabs spumPrefabs;
+    private PlayerState currentState = PlayerState.IDLE;
+
+    private void Awake()
+    {
+        spumPrefabs = GetComponentInParent<SPUM_Prefabs>();
+    }
+
     void Start()
     {
-        
+        spumPrefabs.OverrideControllerInit();
+
+        spumPrefabs.PlayAnimation(PlayerState.IDLE, 0);
     }
 
     
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentState = PlayerState.ATTACK;
+            spumPrefabs.PlayAnimation(currentState, 0); // ATTACK 0 번 애니메이션 재생
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,6 +9,7 @@ public class PlayerController : MonoBehaviour
      */
     [SerializeField] private Transform childTransform;
     [SerializeField] private Rigidbody2D rb;
+    private Collision coll;
 
     private Vector2 moveInput;
     private Vector3 flip;
@@ -23,6 +22,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         childTransform = transform.GetChild(0);
+        coll = GetComponent<Collision>();
         rb = GetComponent<Rigidbody2D>();
         flip = childTransform.localScale;
     }
@@ -36,10 +36,17 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = value.Get<Vector2>();
 
-        if(moveInput.x != 0)
+        if (moveInput.x < 0)
         {
+            flip.x = 1.0f;
             FlipX();
         }
+        else if (moveInput.x > 0)
+        {
+            flip.x = -1.0f;
+            FlipX();
+        }
+
     }
 
     public void OnAttack(InputValue value)
@@ -49,20 +56,25 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputValue value)
     {
-
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+        }
     }
 
     //TODO 이부분이 애니메이션 스크립트로 빠질수있음 현재는 자식 트랜스폼을 가져오는 형태
     public void FlipX()
     {
-        flip.x = -flip.x;
         childTransform.localScale = flip;
     }
 
     void Update()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
+        Move();
+    }
 
-        //rb.linearVelocityX = moveInput.x * walkSpeed;
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
     }
 }
