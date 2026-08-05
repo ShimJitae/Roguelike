4d31334c68b35993f45dbb9823675d67df521a62
SJT <99947889+ShimJitae@users.noreply.github.com>
2026-08-05 15:02:51 +0900

    미니맵 제작중

---
+++ Assets/Jitae/Scripts/MinimapCreator.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MinimapCreator : MonoBehaviour
+{
+    [SerializeField] GameObject minimapCamaraPrefab;
+
+    [SerializeField] GameObject icon_Player;
+    [SerializeField] GameObject icon_Monster;
+    [SerializeField] GameObject icon_Ground;
+}
